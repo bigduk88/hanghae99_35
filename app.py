7baac0e038c285c6092c4e58dd9fc9a642c00220
bigduk88 <hangdeuk@naver.com>
2021-03-02 07:06:04 +0900

    21.03.02 바뀐건 없습니다.

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def sign_in():
             'id': username_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         #키 만들어서 클라이언트에 던져주는 과정
 
         return jsonify({'result': 'success', 'token': token})
